9a7cc0d47dac4730b8ba1d83706b6be35c0c7770
dori1328 <dori1328@gmail.com>
2024-07-18 18:00:58 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+from users import users
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the User API!"
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    return jsonify(users)
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = next((user for user in users if user['id'] == user_id), None)
+    if user is None:
+        return jsonify({'message': 'User not found'}), 404
+    return jsonify(user)
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    new_user = request.get_json()
+    users.append(new_user)
+    return jsonify(new_user), 201
+
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = next((user for user in users if user['id'] == user_id), None)
+    if user is None:
+        return jsonify({'message': 'User not found'}), 404
+    update_data = request.get_json()
+    user.update(update_data)
+    return jsonify(user)
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    global users
+    users = [user for user in users if user['id'] != user_id]
+    return jsonify({'message': 'User deleted'}), 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
